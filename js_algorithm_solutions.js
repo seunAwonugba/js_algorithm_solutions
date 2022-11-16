22f053956ed2666d32bf4ac59e0d79f3ffccf108
seunAwonugba <86803598+seunAwonugba@users.noreply.github.com>
2022-11-16 08:53:08 +0100

    began hacker rank algorithm

--- js_algorithm_solutions.js
+++ js_algorithm_solutions.js
@@ -46,3 +46,37 @@ function logSearch(num, arr) {
 }
 
 console.log(logSearch(10, arrayList));
+
+//hackerrank questions
+// easy
+//1. Complete the function solveMeFirst to compute the sum of two integers.
+function solveMeFirst(a, b) {
+    return a + b;
+}
+console.log(solveMeFirst(4, 10));
+
+//2. simple array sum Given an array of integers, find the sum of its elements.
+
+function arraySum(arr) {
+    var sum = 0;
+    for (let i in arr) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+console.log(arraySum(arrayList));
+
+// 3. A Very Big Sum
+
+function aVeryBigSum(arr) {
+    var sum = 0;
+    for (let i in arr) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+console.log(
+    aVeryBigSum([1000000001, 1000000002, 1000000003, 1000000004, 1000000005])
+);
